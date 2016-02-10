bd0748717f58ddd5b353266233f4ce26947b7e5b
Timelady7 <timelady47@gmail.com>
2016-02-10 23:33:54 +0100

    maj

--- Swifty Companion/ViewController.swift
+++ Swifty Companion/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var inputLogin: UITextField!
+    
+    @IBOutlet weak var searchStudent: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +24,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func searchProfile(sender: UIButton) {
+        print(inputLogin.text)
+    }
 
 }
 
